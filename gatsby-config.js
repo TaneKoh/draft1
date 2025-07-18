0105bc6aaf6d3d04db4aef653d5b620c1f2951e3
Musashi Koh <mkoh@olin.edu>
2025-07-17 21:59:13 -0700

    title fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,10 +4,9 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
-    description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    title: 'Tane Koh',
+    description: 'Tane Koh\'s website portfolio.',
+    siteUrl: 'https://tanekoh.netlify.app/', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
   },
